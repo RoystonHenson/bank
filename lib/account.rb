04b55177756bc22c28dd9abb1e30f9f3e01a6971
Royston Henson <rlqhenson@hotmail.com>
2016-11-29 10:51:04 +0000

    added initialize method with a starting balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,12 @@
+
 class Account
+
+  attr_reader :balance
+
+  STARTING_BALANCE = 0
+
+  def initialize
+    @balance = STARTING_BALANCE
+  end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,11 @@
 require 'account'
 
   describe Account do
+    subject { described_class.new }
 
-    
+      describe '#initialize' do
+        it 'with a starting balance of 0' do
+          expect(subject.balance).to eq(0)
+        end
+      end
   end
